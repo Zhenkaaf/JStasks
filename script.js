6085aeeab3c40956364d60587aef5a5965027fcf
Zhenkaaf <zhenkaaf@gmail.com>
2023-05-12 08:13:03 +0300

    firstUniqChar

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let b = numbers.reduce((prev, item, index, numbers) => {
 });
 
 let c = numbers.reduce((prev, item, index, numbers) => {
-  if(prev < item) {
+  if (prev < item) {
     return item;
   } else {
     return prev;
@@ -18,12 +18,12 @@ let c = numbers.reduce((prev, item, index, numbers) => {
 }, 0);
 
 let f = numbers.reduce((prev, item, index, numbers) => {
-  if(prev[1] < item) {
+  if (prev[1] < item) {
     return [index, item]
   } else {
     return prev
   }
- 
+
 }, [0, numbers[0]]);
 
 console.log(f);
@@ -76,95 +76,59 @@ getWater(river);
 
 
 /* Условия задачи следующие: на вход подается массив интервалов (от и до).
-	К примеру, речь идет о начале и окончании встреч. На выход вы должны вернуть новый массив интервалов,
-	в котором будут помечены все занятые встречами отрезки времени. Если два каких-то митинга пересекаются,
-	то в результат должен попасть один отрезок времени с самым ранним началом и самым поздним окончанием из этих двух встреч.
-	Митинги также считаются пересекающимися, если один начинается ровно тогда, когда закончился предыдущий. */
-  let input = [[1, 3], [2, 6], [8, 10], [15, 18]]; //[[1,6], [8, 10], [15, 18]]
-  let input2 = [[1, 4], [4, 5]]; // [[1, 5]]
-  let asd = [[11, 12], [2, 3], [5, 7], [1, 4], [8, 10], [6, 8]];
-  
-  const mergeIntervals = (data) => {
-    let sortedArr = data.sort((a, b) => {
-      return a[0] - b[0];
-    });
-    console.log(sortedArr);
-    let res = [sortedArr[0]];
-    console.log(res);
-    for (let i = 1; i < sortedArr.length; i++) {
-      let recent = res[res.length - 1];
-      console.log(recent);
-      if (recent[1] >= sortedArr[i][0]) {
-  
-        console.log(sortedArr[i][1]);
-        recent[1] = Math.max(recent[1], sortedArr[i][1]);
-      }
-      else {
-        res.push(sortedArr[i]);
-      }
+  К примеру, речь идет о начале и окончании встреч. На выход вы должны вернуть новый массив интервалов,
+  в котором будут помечены все занятые встречами отрезки времени. Если два каких-то митинга пересекаются,
+  то в результат должен попасть один отрезок времени с самым ранним началом и самым поздним окончанием из этих двух встреч.
+  Митинги также считаются пересекающимися, если один начинается ровно тогда, когда закончился предыдущий. */
+let input = [[1, 3], [2, 6], [8, 10], [15, 18]]; //[[1,6], [8, 10], [15, 18]]
+let input2 = [[1, 4], [4, 5]]; // [[1, 5]]
+let asd = [[11, 12], [2, 3], [5, 7], [1, 4], [8, 10], [6, 8]];
+
+const mergeIntervals = (data) => {
+  let sortedArr = data.sort((a, b) => {
+    return a[0] - b[0];
+  });
+  console.log(sortedArr);
+  let res = [sortedArr[0]];
+  console.log(res);
+  for (let i = 1; i < sortedArr.length; i++) {
+    let recent = res[res.length - 1];
+    console.log(recent);
+    if (recent[1] >= sortedArr[i][0]) {
+
+      console.log(sortedArr[i][1]);
+      recent[1] = Math.max(recent[1], sortedArr[i][1]);
     }
-    console.log('res', res);
-  };
-  mergeIntervals(input);
-  /* const mergeIntervals = (data) => {
-    console.log(data);
-    let sortedArr = data.sort((a, b) => {
-      return a[0] - b[0];
-    });
-    console.log("Sorted array: ", sortedArr);
-  
-    let res = [sortedArr[0]];
-    for (let i = 0; i < sortedArr.length; i++) {
-      let recent = res[res.length - 1];
-      console.log(recent);
-      if (recent[1] >= sortedArr[i][0]) {
-        recent[1] = sortedArr[i][1];
-      }
-      else {
-        console.log('else');
-        res.push(sortedArr[i]);
-      }
+    else {
+      res.push(sortedArr[i]);
     }
-    console.log('res', res);
-  };
-  console.log(asd);
-  mergeIntervals(asd); */
-
-
-
-
-
-
-
-
-
-  /* будем использовать алгоритм с созданием хешмапа. По условиям: необходимо написать функцию, которая принимает на вход два массива с числами. На выходе мы должны вернуть новый массив, который будет в себе содержать только те элементы, которые встречались в обоих массивах. Важно заметить, что если в первом массиве у нас было, например, три двойки, а во втором массиве было четыре двойки — в результирующем массиве их должно быть три. Так как три двойки встречались и в первом, и во втором массиве. */
-
-
-  const inp1 = [1, 2, 2, 1];
-  const inp2 = [2, 2];
-  
-  const inp3 = [4, 9, 5];
-  const inp4 = [9, 4, 9, 8, 4];
-  
-  const intersect = function (arr1, arr2) {
-    const map = {};
-    const res = [];
-    for (let i = 0; i < arr1.length; i++) {
-      map[arr1[i]] ? map[arr1[i]] = map[arr1[i]] + 1 : map[arr1[i]] = 1;
-      //map[arr1[i]] = map[arr1[i]] ? map[arr1[i]] + 1 : 1;
+  }
+  console.log('res', res);
+};
+mergeIntervals(input);
+/* const mergeIntervals = (data) => {
+  console.log(data);
+  let sortedArr = data.sort((a, b) => {
+    return a[0] - b[0];
+  });
+  console.log("Sorted array: ", sortedArr);
+ 
+  let res = [sortedArr[0]];
+  for (let i = 0; i < sortedArr.length; i++) {
+    let recent = res[res.length - 1];
+    console.log(recent);
+    if (recent[1] >= sortedArr[i][0]) {
+      recent[1] = sortedArr[i][1];
     }
-    for (let k = 0; k < arr2.length; k++) {
-      if (arr2[k] in map && map[arr2[k]] > 0) {
-        res.push(arr2[k]);
-        map[arr2[k]] -= 1;
-      }
+    else {
+      console.log('else');
+      res.push(sortedArr[i]);
     }
-    console.log(map);
-    console.log(res);
-  };
-  
-  intersect(inp3, inp4);
+  }
+  console.log('res', res);
+};
+console.log(asd);
+mergeIntervals(asd); */
 
 
 
@@ -174,7 +138,43 @@ getWater(river);
 
 
 
-  /* задачи по поиску чисел из последовательности Фибоначчи: через цикл избыточный, через цикл оптимальный и через рекурсию. Вариант с рекурсией часто спрашивают на собеседованиях. И используют для изучения самой рекурсии, т.к. для того чтобы понять, что такое рекурсия — надо знать, что такое рекурсия :). Вариант решения через цикл будет работать значительно быстрее варианта с рекурсией. */
+/* будем использовать алгоритм с созданием хешмапа. По условиям: необходимо написать функцию, которая принимает на вход два массива с числами. На выходе мы должны вернуть новый массив, который будет в себе содержать только те элементы, которые встречались в обоих массивах. Важно заметить, что если в первом массиве у нас было, например, три двойки, а во втором массиве было четыре двойки — в результирующем массиве их должно быть три. Так как три двойки встречались и в первом, и во втором массиве. */
+
+
+const inp1 = [1, 2, 2, 1];
+const inp2 = [2, 2];
+
+const inp3 = [4, 9, 5];
+const inp4 = [9, 4, 9, 8, 4];
+
+const intersect = function (arr1, arr2) {
+  const map = {};
+  const res = [];
+  for (let i = 0; i < arr1.length; i++) {
+    map[arr1[i]] ? map[arr1[i]] = map[arr1[i]] + 1 : map[arr1[i]] = 1;
+    //map[arr1[i]] = map[arr1[i]] ? map[arr1[i]] + 1 : 1;
+  }
+  for (let k = 0; k < arr2.length; k++) {
+    if (arr2[k] in map && map[arr2[k]] > 0) {
+      res.push(arr2[k]);
+      map[arr2[k]] -= 1;
+    }
+  }
+  console.log(map);
+  console.log(res);
+};
+
+intersect(inp3, inp4);
+
+
+
+
+
+
+
+
+
+/* задачи по поиску чисел из последовательности Фибоначчи: через цикл избыточный, через цикл оптимальный и через рекурсию. Вариант с рекурсией часто спрашивают на собеседованиях. И используют для изучения самой рекурсии, т.к. для того чтобы понять, что такое рекурсия — надо знать, что такое рекурсия :). Вариант решения через цикл будет работать значительно быстрее варианта с рекурсией. */
 
 function fibonacci(num) {
   const result = [0, 1];
@@ -189,7 +189,7 @@ console.log(fibonacci(7));
 
 
 function fibrecursia(num) {
-  if(num < 2) {
+  if (num < 2) {
     return num;
   }
   return fibrecursia(num - 1) + fibrecursia(num - 2);
@@ -206,21 +206,21 @@ console.log(fibrecursia(10));
 
 /* Кирпичная стена (554. Brick Wall). На LeetCode она Medium уровня сложности. Эту задачу нам прислал один из подписчиков, которому она попалась на собеседовании. Поэтому спасибо за вашу активность, присылайте еще! По условию: у нас есть прямоугольная кирпичная стена высотой n кирпичей. Все кирпичи одинаковой высоты, но могут быть различной ширины. В каждом ряду может быть разное количество различных кирпичей, но ширина всех рядов кирпичей всегда будет одинакова. Если провести вертикальную линию по стене, то такая линия будет пересекать какое-то количество кирпичей. Необходимо найти, какое минимальное количество кирпичей может пересекать такая вертикальная линия. Важно отметить, что если линия проходит на стыке двух кирпичей, то это не считается пересечением. Еще один момент — мы не можем провести линию с одной из сторон стены — линия должна быть именно во внутренней части стены. */
 
-let wall = [[1,2,2,1], [3,1,2], [1,3,2], [2,4], [3,1,2], [1,3,1,1]];
+let wall = [[1, 2, 2, 1], [3, 1, 2], [1, 3, 2], [2, 4], [3, 1, 2], [1, 3, 1, 1]];
 
 const leastBricks = (wall) => {
- const map = {};
- let max = 0;
- wall.forEach(row => {
-  let sum = 0;
-  for (let n =0; n < row.length - 1; n++) {
-    sum += row[n];
-    map[sum] = map[sum] ? map[sum] + 1 : 1;
-    max = Math.max(map[sum], max);
-  }
- })
- console.log(map);
- return wall.length - max;
+  const map = {};
+  let max = 0;
+  wall.forEach(row => {
+    let sum = 0;
+    for (let n = 0; n < row.length - 1; n++) {
+      sum += row[n];
+      map[sum] = map[sum] ? map[sum] + 1 : 1;
+      max = Math.max(map[sum], max);
+    }
+  })
+  console.log(map);
+  return wall.length - max;
 };
 console.log(leastBricks(wall));
 
@@ -248,9 +248,9 @@ const checkBrackets = (s) => {
     ']': '['
   };
   const closedBrackets = [')', ']', '}'];
-  for(let i = 0; i < s.length; i++) {
-    if(closedBrackets.includes(s[i])) {
-      if(typeOfBrackets[s[i]] != stack.pop()) return false;
+  for (let i = 0; i < s.length; i++) {
+    if (closedBrackets.includes(s[i])) {
+      if (typeOfBrackets[s[i]] != stack.pop()) return false;
     } else {
       stack.push(s[i]);
     }
@@ -276,9 +276,9 @@ console.log(s6, checkBrackets(s6));
 let arr = [7, 1, 5, 3, 6, 4];
 let fn = () => {
   let profit = 0;
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] < arr[i+1]) {
-      profit += arr[i+1] - arr[i];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < arr[i + 1]) {
+      profit += arr[i + 1] - arr[i];
     }
   }
   console.log(profit);
@@ -308,3 +308,34 @@ fn();
 
   console.log(merge(input));
   console.log(merge(input2)); */
+
+
+
+
+
+/* First Unique Character in a String — Первый уникальный символ в строке. Условия следующие: нам необходимо написать функцию, которая принимает в качестве аргумента строку и возвращает индекс первого уникального символа. Если мы не найдем ни одного уникального символа, то функция должна вернуть -1. Дополнительные ограничения: 1) длина строки может быть от 1 до 10^5; 2) строка содержит только буквы английского алфавита в нижнем регистре. В этом видео я решил использовать new Map для создания хешмапов. Вы можете вполне использовать и обычный объект — для данной задачи разницы нет. Выбрал Map, чтоб было, как "по классике". */
+
+
+
+
+
+const uniqStr1 = 'leetcode'; //0
+const uniqStr2 = 'loveleetcode'; //2
+const uniqStr3 = 'aabb'; //-1
+
+const firstUniqChar = (str) => {
+  let map = new Map();
+  for (let i = 0; i < str.length; i++) {
+    map.set(str[i], map.has(str[i]) ? map.get(str[i]) + 1 : 1);
+  }
+  console.log(map);
+  for (let [char, quantity] of map) {
+    if (quantity == 1) {
+      return str.indexOf(char);
+    }
+  }
+  return -1;
+};
+console.log('uniqStr1---', firstUniqChar(uniqStr1));
+console.log('uniqStr2---', firstUniqChar(uniqStr2));
+console.log('uniqStr3---', firstUniqChar(uniqStr3));
